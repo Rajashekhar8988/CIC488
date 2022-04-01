789b614f034d1cd588520f4f60c4ca06d8408eec
Raja <rajashekharraj3@gmail.com>
2022-04-02 02:36:27 +0530

    changes made

--- arithmetic.sh
+++ arithmetic.sh
@@ -9,5 +9,10 @@ read b
 echo "Enter third number"
 read c
 
-compute=$(($a+$b*$c))
-echo $compute
+echo "The value of x is : "
+x=$(($a+$b*$c))
+echo $x
+
+echo "The value of y is : "
+y=$(($a*$b+$c))
+echo $y
