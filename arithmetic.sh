bf55bb54b3d902ad88f2031197b60a9edaddf71d
Janga Rajashekhar Reddy <rajashekharraj3@gmail.com>
2022-04-04 20:07:39 +0530

    added extension file

--- arithmetic.sh
+++ arithmetic.sh
@@ -9,22 +9,34 @@ read b
 echo "Enter third number"
 read c
 
-echo "The value of x is : "
-x=$(($a+$b*$c))
-echo $x
+echo "The value of result1 is : "
+result1=$(($a+$b*$c))
+echo $result1
 
-echo "The value of y is : "
-y=$(($a*$b+$c))
-echo $y
+echo "The value of result2 is : "
+result2=$(($a*$b+$c))
+echo $result2
 
-echo "The value of z is : "
-z=$(($c*$a/$b))
-echo $z
+echo "The value of result3 is : "
+result3=$(($c*$a/$b))
+echo $result3
 
-echo "The value of l is : "
-l=$(($a%$b+$c))
-echo $l
+echo "The value of result4 is : "
+result4=$(($a%$b+$c))
+echo $result4
 
-declare -A Values
-Values={1:"$x",2:"$y",3:"z"}
-echo "Values of arithmetic : " ${Values.values()}
+declare -A dict
+dict[first]="$result1"
+dict[second]="$result2"
+dict[third]="$result3"
+dict[fourth]="$result4"
+
+echo "1st output : " ${dict[first]}
+echo "2nd output : " ${dict[second]}
+echo "3rd output : " ${dict[third]}
+echo "4th output : " ${dict[fourth]}
+
+echo ${dict[@]}
+
+ascsortingn = {k: v for k, v in sorted(dict.items(), key= lamda v: v[1])}
+ascsortingn = {k: v for k, v in sorted(dict.items(), key= lamda v: v[1], reverse=True)}
